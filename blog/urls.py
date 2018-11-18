ffeb56d6138796348ab7977767d6b1bff75c4a05
Bektursun <samatuulu11@gmail.com>
2018-11-19 03:54:28 +0600

    blogengine

---
+++ blog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+
+
+from django.urls import path, include
+from . import views
+
+from .views import*
+
+urlpatterns = [
+    path('', posts_list, name = 'post_list_url'),
+    path('post/create/', PostCreate.as_view(), name = 'post_create_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/create', TagCreate.as_view(), name = 'tag_create_url'),
+    path('tag/<str:slug>', TagDetail.as_view(), name = 'tag_detail_url'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', views.Home.as_view(), name='home'),
+
+]
